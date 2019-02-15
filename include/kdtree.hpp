d4b7def8905f0600d9edc259e4a94d9a8f6a1669
Harald August Bergesen <bergesenha@gmail.com>
2019-02-15 17:50:19 +0100

    Add new class 'kdtree' with basic memory layout. 	modified:   include/kdtree.hpp

--- include/kdtree.hpp
+++ include/kdtree.hpp
@@ -1,11 +1,38 @@
 #pragma once
 
 #include "point_traits.hpp"
+#include "stable_container.hpp"
 
 namespace useful
 {
 namespace multidim
 {
+template <class PointType>
+class kdtree
+{
+    struct node;
 
-}
+public:
+    typedef typename stable_vector<node>::size_type size_type;
+
+private:
+    struct node
+    {
+        PointType value;
+        stable_vector<node>* storage_ref_;
+        size_type smaller;
+        size_type bigger;
+    };
+
+public:
+    bool
+    empty() const
+    {
+        return storage_.size() == 0;
+    }
+
+private:
+    stable_vector<node> storage_;
+};
+} // namespace multidim
 } // namespace useful
