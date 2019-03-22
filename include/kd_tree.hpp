b6c8f9f35dece40da3ac60f45b17a0d63388059d
Harald August Bergesen <bergesenha@gmail.com>
2019-03-22 21:22:45 +0100

    Cleanup. 	modified:   include/kd_tree.hpp

--- include/kd_tree.hpp
+++ include/kd_tree.hpp
@@ -9,48 +9,6 @@ namespace useful
 {
 namespace multidim
 {
-namespace super_index_detail
-{
-template <std::size_t Dim>
-struct order
-{
-};
-
-template <class IndexSequence>
-struct holder;
-
-
-template <std::size_t... Sequence>
-struct holder<std::index_sequence<Sequence...>> : order<Sequence>...
-{
-};
-
-} // namespace super_index_detail
-
-template <class PointType>
-class super_index
-    : super_index_detail::holder<
-          std::make_index_sequence<point_traits<PointType>::dimensions>>
-{
-public:
-    template <class Iterator>
-    super_index(Iterator first, Iterator last)
-    {
-    }
-
-private:
-    template <std::size_t Dim>
-    void
-    sort_on_dimension()
-    {
-        super_index_detail::order<Dim>* value_holder =
-            static_cast<super_index_detail::order<Dim>*>(this);
-    }
-
-private:
-};
-
-
 template <class PointType>
 class kd_tree
 {
