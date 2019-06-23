f2bd6dac7687af573234dc297c34f055cbed4765
Harald August Bergesen <bergesenha@gmail.com>
2019-06-23 17:38:32 +0200

    add member function 'link' to class 'dag'. 	modified:   include/dag.hpp

--- include/dag.hpp
+++ include/dag.hpp
@@ -17,6 +17,8 @@ public:
 
     size_type insert(const T& node);
 
+    void link(size_type from, size_type to);
+
 private:
     stable_vector<T> nodes_;
     stable_vector<small_vector<size_type>> from_links_;
@@ -31,6 +33,16 @@ template <class T>
 typename dag<T>::size_type
 dag<T>::insert(const T& node)
 {
+    from_links_.push_back(small_vector<size_type>());
+    to_links_.push_back(small_vector<size_type>());
     return nodes_.push_back(node);
+}
+
+template <class T>
+void
+dag<T>::link(typename dag<T>::size_type from, typename dag<T>::size_type to)
+{
+    from_links_[from].push_back(to);
+    to_links_[to].push_back(from);
 }
 } // namespace useful
