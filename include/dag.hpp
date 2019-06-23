dd5b8258a8c8242ee2bc071044d0119bce896201
Harald August Bergesen <bergesenha@gmail.com>
2019-06-23 17:26:23 +0200

    implement member function 'insert'.

--- include/dag.hpp
+++ include/dag.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "handle_map.hpp"
+#include "stable_container.hpp"
+#include "small_vector.hpp"
 
 
 namespace useful
@@ -9,9 +10,27 @@ template <class T>
 class dag
 {
 public:
+    typedef typename stable_vector<T>::size_type size_type;
+
+public:
     dag() = default;
 
+    size_type insert(const T& node);
+
 private:
-    handle_map<T> nodes_;
+    stable_vector<T> nodes_;
+    stable_vector<small_vector<size_type>> from_links_;
+    stable_vector<small_vector<size_type>> to_links_;
 };
+} // namespace useful
+
+
+namespace useful
+{
+template <class T>
+typename dag<T>::size_type
+dag<T>::insert(const T& node)
+{
+    return nodes_.push_back(node);
+}
 } // namespace useful
