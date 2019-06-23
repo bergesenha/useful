ce8877f96310791e57215265b1d8673130fa3e5a
Harald August Bergesen <bergesenha@gmail.com>
2019-06-23 17:23:12 +0200

    Implement data layout of class 'dag'.

--- include/dag.hpp
+++ include/dag.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include "handle_map.hpp"
+
 
 namespace useful
 {
+template <class T>
+class dag
+{
+public:
+    dag() = default;
 
-}
+private:
+    handle_map<T> nodes_;
+};
+} // namespace useful
